27a13d484e99c74be0abb8f7ac5e9fbeb314b4bb
Wilson Montelo <wilson.montelo@vibedesenv.com>
2019-10-09 19:21:06 -0300

    Adicionando operações

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,6 +1,7 @@
 class CalcController {
     
     constructor(){
+        this._operation = [];
         this._locale = 'pt-bR';
         this._displayCalEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
@@ -19,10 +20,80 @@ class CalcController {
         }, 1000);
     }
 
+    addEventListenerAll(element, events, fn){
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false);
+        });
+    }
+
+    clearAll(){
+        this._operation = [];
+    }
+
+    clearEntry(){
+        this._operation.pop();
+    }
+
+    addOperation(value){
+        this._operation.push(value);
+        console.log(this._operation);
+    }
+
+    setError(){
+        this.displayCalc = "Error";
+    }
+
+    execBtn(value){
+        switch (value){
+            case 'ac':
+                this.clearAll();
+                break;
+            case 'ce':
+                this.clearEntry();
+                break;
+            case 'soma':
+                break;
+            case 'subtracao':
+                break;
+            case 'divisao':
+                break;
+            case 'multiplicacao':
+                break;
+            case 'porcento':
+                break;
+            case 'igual':
+                break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+            default:
+                this.setError();
+                break;
+        }
+    }
+
     initButtonsEvents(){
-        let buttons = document.querySelector("#buttons > g.btn-9");
-        buttons.addEventListener('click', e=>{
-            console.log(e);
+        
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+        
+        buttons.forEach((btn, index)=>{
+            this.addEventListenerAll(btn, "click drag", e=>{
+                console.log(btn.className.baseVal.replace("btn-", ""));
+                this.execBtn();
+            });
+
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e=> {
+                btn.style.cursor = "pointer";
+            });
         });
     }
 
